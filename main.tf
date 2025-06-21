1b0a0066282244c9f7656715e4d154ef000b4662
Andrew McLaughlin <andrew@thedataplatform.io>
2025-06-21 10:01:31 -0400

    update storage name to use result from random string

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "this" {
 }
 
 resource "azurerm_storage_account" "this" {
-  name                     = lower("storageaccount${random_string.random}")
+  name                     = lower("storageaccount${random_string.random.result}")
   resource_group_name      = azurerm_resource_group.this.name
   location                 = azurerm_resource_group.this.location
   account_tier             = "Standard"
