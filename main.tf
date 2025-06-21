c327cff3cd4130e8583f86fb058f3788475374b1
Andrew McLaughlin <andrew@thedataplatform.io>
2025-06-21 09:44:31 -0400

    initial commit

---
+++ main.tf
@@ -0,0 +1,18 @@
+resource "azurerm_resource_group" "this" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
+}
+
+resource "azurerm_storage_account" "this" {
+  name                     = lower("storageaccount${random_string.random}")
+  resource_group_name      = azurerm_resource_group.this.name
+  location                 = azurerm_resource_group.this.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  depends_on               = [ random_string.random ]
+}
+
+resource "random_string" "random" {
+  length           = 10
+  special          = false
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  description   = "Name for the resource group"
+  type          = string
+  default       = "quick-sandbox-rg"
+}
+
+variable "resource_group_location" {
+  description   = "Location for the resource group"
+  type          = string
+  default       = "East US 2"
+}
